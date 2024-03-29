b1c3d43a6b9647fc70e2baf17da56e03305bc77d
Hastin Modi <hastinmodi@gmail.com>
2024-03-29 15:38:46 -0700

    eventlet issue

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-import eventlet
-eventlet.monkey_patch()
+# import eventlet
+# eventlet.monkey_patch()
 from flask import Flask, request, jsonify
 from flask_cors import cross_origin
 from main_llm import find_facts
